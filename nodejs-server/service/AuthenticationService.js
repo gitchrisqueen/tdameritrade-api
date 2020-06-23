861d0d71c3595553d3f577f2c98df4d4b7236e53
SwaggerHub <SwaggerHub@smartbear.com>
2020-06-23 18:08:06 +0000

    API definition transferred by SwaggerHub

--- nodejs-server/service/AuthenticationService.js
+++ nodejs-server/service/AuthenticationService.js
@@ -11,11 +11,24 @@
  * code String  (optional)
  * client_id Integer  (optional)
  * redirect_uri String  (optional)
- * no response value expected for this operation
+ * returns EASObject
  **/
 exports.oauth2TokenPOST = function(grant_type,refresh_token,access_type,code,client_id,redirect_uri) {
   return new Promise(function(resolve, reject) {
-    resolve();
+    var examples = {};
+    examples['application/json'] = {
+  "access_token" : "access_token",
+  "refresh_token_expires_in" : 6,
+  "refresh_token" : "refresh_token",
+  "scope" : "scope",
+  "token_type" : "token_type",
+  "expires_in" : 0
+};
+    if (Object.keys(examples).length > 0) {
+      resolve(examples[Object.keys(examples)[0]]);
+    } else {
+      resolve();
+    }
   });
 }
 
